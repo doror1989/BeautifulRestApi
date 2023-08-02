4a192fa691312beb74b4f06ff1eb00b90ea9df01
doror1989 <doror1989@gmail.com>
2023-08-02 15:32:14 +0300

    Update and rename UsersRouteShould.cs to test.py

---
+++ test/test.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+class test:
+
+    def is_authenticated(func):
+        def wrapper(*args, **kwargs):
+            return True
+
+    @app.route("/protected_route/")
+    @is_authenticated
+    def protected_route(user,id):
+        return jsonify({"message": f"Hello, {user.username}! This is a protected route."})
