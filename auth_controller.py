ec82bbfb41915f40426167be7bd64928f2de1d3c
doror1989 <doror1989@gmail.com>
2023-08-02 16:13:14 +0300

    Update auth_controller.py

--- auth_controller.py
+++ auth_controller.py
@@ -11,3 +11,8 @@ class test:
     @is_authenticated
     def protected_route(user,id):
         return jsonify({"message": f"Hello, {user.username}! This is a protected route."})
+
+
+    @app.route("/not protected_route/")
+    def not_protected_route(user,id):
+        return jsonify({"message": f"Hello, {user.username}! This is not a protected route."})
