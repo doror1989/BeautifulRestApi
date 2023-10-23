d34ae99b558c7223051ca4a2d91d74d6f3c26b30
doror1989 <doror1989@gmail.com>
2023-10-23 16:03:28 +0300

    Update auth_controller.py

--- auth_controller.py
+++ auth_controller.py
@@ -11,13 +11,11 @@ class test:
         loan: int = 50
         logger.info(f"{loan}")
     
-    @is_authenticated
     @app.route("/protected_route/:id")
     def protected_route(id):
         return jsonify({"message": f"Hello, {id}! This is a protected route."})
 
     
-    @is_authenticated
     @app.route("/protected_route_2/:email")
     def protected_route_2(email):
         return jsonify({"message": f"Hello, {user.username}! This is a protected route."})
